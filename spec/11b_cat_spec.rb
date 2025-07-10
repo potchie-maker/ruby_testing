10b923f95450cce87d20faee43a1074bee34319d
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2025-07-10 13:55:13 -0400

    Complete 11b_cat_spec.rb

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:fluffers) { described_class.new('Fluffers') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat shares method name' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(fluffers).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(fluffers).to be_hiding
   end
 end
